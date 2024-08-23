47f3e56d850c1f4bb2563086cfd599f173ad405a
PankajaBalasooriya <pankajabalasooriya566@gmail.com>
2024-08-23 11:43:54 +0530

    Code Update

--- Code/DUST.h
+++
@@ -1,6 +0,0 @@
-#ifndef DUST_H
-#define DUST_H
-
-double dust_density(int measurePin,int ledPower);
-
-#endif /* DUST_H */
--- Code/GAS.h
+++
@@ -1,6 +0,0 @@
-#ifndef GAS_H
-#define GAS_H
-
-double gas_level(int gas_pin);
-
-#endif /* GAS_H */
